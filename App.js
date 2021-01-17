7a83cba27cc104c99fa41af24170d67e8f388311
hamam <hamam@jakpat.net>
2021-01-17 16:40:02 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to React Native </Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
