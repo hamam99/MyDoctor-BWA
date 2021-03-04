4fe4e923c3accb4bf7fdb6f01d4508b7d103ae6a
hamam <hamam@jakpat.net>
2021-03-05 04:26:45 +0700

    develop : Refactoring color

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { colors } from '../../../utils';
 
 const Button = ({type, title, onPress = {}}) => {
   return (
@@ -12,13 +13,13 @@ export default Button;
 
 const styles = StyleSheet.create({
   container: (type) =>({
-    backgroundColor:type === 'secondary' ? 'white' : '#0BCAD4',
+    backgroundColor:type === 'secondary' ? colors.button.secondary.background : colors.button.primary.background,
     borderRadius:10,
     paddingVertical:10,
   }),
   title: (type) => ({
     fontSize:   18,
-    color: type === 'secondary' ? 'black' : 'white',
+    color: type === 'secondary' ? colors.button.secondary.text : colors.button.primary.text,
     textAlign:'center',
     fontFamily:'Nunito-SemiBold',
   }),
