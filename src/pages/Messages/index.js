d31f018ba2d3c5fcbcbf80ab11e4b6294a736803
Hamam <zero.saturnus@gmail.com>
2021-06-12 21:58:47 +0700

    develop: Update messages screen

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import ListDoctor from '../../components/molecules/ListDoctor';
 import { colors, fonts } from '../../utils';
 
 const Messages = ({navigation}) => {
   return (
-    <View>
-      <Text style={styles.title}>
-        {'Messzsges Pages'}
-      </Text>
+    <View style={styles.page}>
+      <View style={styles.content}>
+        <Text style={styles.title}> {'Messages'} </Text>
+        <ListDoctor/>
+        <ListDoctor/>
+        <ListDoctor/>
+      </View>
     </View>
   );
 };
@@ -16,14 +20,17 @@ export default Messages;
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    padding:40,
-    justifyContent:'space-between',
-    backgroundColor:colors.white,
+    backgroundColor:colors.secondary,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex:1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius:20,
   },
   title: {
-    fontSize: 28,
-    marginTop: 91,
-    color:'white',
+    fontSize: 20,
+    color:colors.text.primary,
     fontFamily: fonts.primary[600],
   },
 });
